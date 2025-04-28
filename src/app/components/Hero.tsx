87ebbfd0e91f6a585a81ce2734b1464c6724a7f0
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-04-28 22:16:07 +0530

    Animation practice 2

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,6 +3,7 @@ import { useRef } from "react";
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from "gsap/ScrollTrigger"
+import Image from "next/image";
 gsap.registerPlugin(ScrollTrigger)
 
 
@@ -10,6 +11,8 @@ export default function Hero() {
 
     const textRef = useRef(null);
     const screenRef = useRef(null);
+    const cursorRef = useRef(null);
+    const text2Ref = useRef(null);
 
     useGSAP(() => {
         const tl = gsap.timeline({
@@ -24,14 +27,41 @@ export default function Hero() {
 
         tl.to(textRef.current, {
             transform: "translateX(-60%)",
-        })
+        });
+
+        document.addEventListener('mousemove', (e) => {
+            gsap.to(cursorRef.current, {
+                x: e.clientX - 8,
+                y: e.clientY - 6,
+            });
+        });
     });
+
+    const handleGrow = () => {
+        gsap.to(cursorRef.current, {
+            scale: 4,
+        });
+    }
+
+    const handleShrink = () => {
+        gsap.to(cursorRef.current, {
+            scale: 1,
+
+        });
+    }
 
     return (
         <>
-            <section className='w-full h-screen bg-zinc-900'></section>
+            <div ref={cursorRef} className="bg-white w-6 rounded-full h-6 fixed mix-blend-difference z-40"></div>
+            <section className='w-full h-screen bg-zinc-900 flex justify-center items-center'>
+                <div onMouseEnter={handleGrow} onMouseLeave={handleShrink} ref={text2Ref} className="w-max h-max cursor-pointer flex relative">
+                    <h1 className="text-5xl font-bold text-white">HOVER ME</h1>
+                    <div className="w-64 h-12 absolute z-50">
+                    </div>
+                </div>
+            </section>
             <section ref={screenRef} className='w-full h-screen flex items-center bg-[#fff6ea] overflow-hidden'>
-                <h1 ref={textRef} className="text-[35vw] text-zinc-900 font-bold">EXPERIENCES</h1>
+                <h1 ref={textRef} className="text-[35vw] text-[#442600] font-bold">EXPERIENCES</h1>
             </section>
             <section className='w-full h-screen bg-slate-900'></section>
         </>
